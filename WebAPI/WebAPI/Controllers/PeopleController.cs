768e006b10ca74c402e37577eb4aa540f381d0a4
PilnikMatus <61269394+PilnikMatus@users.noreply.github.com>
2020-04-15 14:31:09 +0200

    správnej soubor
    matuš

---
+++ WebAPI/WebAPI/Controllers/PeopleController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PeopleController : ApiController
+    {
+        private PersonRepository repository = new PersonRepository();
+
+        // GET: api/People
+        public IEnumerable<Person> Get()
+        {
+            return this.repository.FindAll();
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            return this.repository.FindById(id);
+        }
+
+        // POST: api/People
+        public void Post([FromBody]Person value)
+        {
+            this.repository.Create(value);
+        }
+
+        // PUT: api/People/5
+        public void Put(int id, [FromBody]Person value)
+        {
+            value.Id = id;
+            this.repository.Update(value);
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+            Person person = this.repository.FindById(id);
+            this.repository.Delete(person);
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/PersonRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class PersonRepository
+    {
+        private MyContext context = new MyContext();
+
+        public List<Person> FindAll()
+        {
+            return this.context.People.ToList();
+        }
+
+        public Person FindById(int id)
+        {
+            //return this.context.People.Where(x => x.Id == id).FirstOrDefault();
+            return this.context.People.Find(id);
+        }
+
+        public void Create(Person person)
+        {
+            this.context.People.Add(person);
+            this.context.SaveChanges();
+        }
+
+        public void Update(Person person)
+        {
+            /*Person current = this.FindById(person.Id);
+
+            current.Name = person.Name;
+            current.Surname = person.Surname;
+            current.Age = person.Age;
+
+            this.context.SaveChanges();*/
+
+            this.context.Entry(person).State = System.Data.Entity.EntityState.Modified;
+            this.context.SaveChanges();
+        }
+
+        public void Delete(Person person)
+        {
+            this.context.People.Remove(person);
+            this.context.SaveChanges();
+        }
+    }
+}
